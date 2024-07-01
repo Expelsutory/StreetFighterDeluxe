f7a7686c02e1e6d622ddbda7721523f81295302c
expelsutory <expelsutory@gmail.com>
2024-07-01 02:06:16 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,67 @@
+const canvas = document.querySelector('canvas');
+const content = canvas.getContext('2d');
+
+canvas.width = 1024
+canvas.height = 576
+
+content.fillRect(0, 0, canvas.width, canvas.height)
+
+const gravity = 0.2
+
+class Sprite {
+    constructor({position, velocity}) {
+        this.position = position
+        this.velocity = velocity
+        this.height = 150
+    }
+
+    draw() {
+        content.fillStyle = 'yellow'
+        content.fillRect(this.position.x, this.position.y, 50, this.height)
+    }
+
+    update(){
+        this.draw()
+        this.position.y += this.velocity.y
+
+        if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+            this.velocity.y = 0
+        } else this.velocity.y += gravity
+    }
+}
+
+const player = new Sprite({
+    position: {
+        x: 0,
+        y: 0
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    }
+})
+
+const enemy = new Sprite({
+    position: {
+        x: 400,
+        y: 0
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    }
+})
+
+enemy.draw()
+
+console.log(player)
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    content.fillStyle = 'black'
+    content.fillRect(0, 0, canvas.width, canvas.height)
+    player.update()
+    enemy.update()
+}
+
+animate()
